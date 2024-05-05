b094b78491b409fffcda22553cad4b2b5e19cd26
Fairushin Tagir <tfayrushin@bk.ru>
2024-05-05 12:58:23 +0300

    tagir

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	//new Line
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);//edit
 	}
 
 }
